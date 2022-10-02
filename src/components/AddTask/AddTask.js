64a0cf80761063987cf8dfe680d7485db63442aa
Lindsey Bieda <hazardous.narayan@gmail.com>
2022-10-01 21:04:45 -0700

    Add a modal form for the task

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,7 +1,45 @@
-import { Button } from '@chakra-ui/react'
+import { Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react'
 
 function AddTask() {
-  return (<Button width="100%">Add Task</Button>);
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Button width="100%" onClick={onOpen}>Add Task</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add Task</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl isRequired>
+              <FormLabel>Task</FormLabel>
+              <Input placeholder='A cleaning task' />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Add
+            </Button>
+            <Button variant="ghost" onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
 
 export default AddTask;
