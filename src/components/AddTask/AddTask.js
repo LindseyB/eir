d2b9f1ef94d7f704d29e150751f5fcc4f716010e
Lindsey Bieda <hazardous.narayan@gmail.com>
2022-10-01 21:42:00 -0700

    Start adding form stuff

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,5 +1,6 @@
 import { Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   Modal,
@@ -12,28 +13,68 @@ import { Button,
   useDisclosure,
 } from '@chakra-ui/react'
 
+import { Field, Form, Formik } from 'formik';
+
 function AddTask() {
   const { isOpen, onOpen, onClose } = useDisclosure()
+
+  function validateTask(value) {
+    let error;
+    if (!value) {
+      error = "Task is required";
+    }
+    return error;
+  }
+
+  function renderForm() {
+    return (
+      <Formik
+        initialValues={{ task: '' }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            actions.setSubmitting(false)
+            onClose()
+          }, 1000)
+        }}
+      >
+        {(props) => (
+          <Form>
+            <Field name='task' validate={validateTask}>
+              {({ field, form }) => (
+                <FormControl isInvalid={form.errors.task && form.touched.task}>
+                  <FormLabel>Task</FormLabel>
+                  <Input {...field} placeholder='task' required/>
+                  <FormErrorMessage>{form.errors.task}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Button
+              mt={4}
+              colorScheme='teal'
+              isLoading={props.isSubmitting}
+              type='submit'
+            >
+              Submit
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    )
+  }
 
   return (
     <>
       <Button width="100%" onClick={onOpen}>Add Task</Button>
-
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Add Task</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <FormControl isRequired>
-              <FormLabel>Task</FormLabel>
-              <Input placeholder='A cleaning task' />
-            </FormControl>
+            { renderForm() }
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Add
-            </Button>
             <Button variant="ghost" onClick={onClose}>Cancel</Button>
           </ModalFooter>
         </ModalContent>
